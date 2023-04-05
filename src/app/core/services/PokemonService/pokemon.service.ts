08f7fa908356b36c4fa604af865443a9a98eeab2
Florian Lefèvre <florian.lefevre@diiage.org>
2023-04-05 16:44:52 +0200

    first test

--- src/app/core/services/PokemonService/pokemon.service.ts
+++ src/app/core/services/PokemonService/pokemon.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
-import {PokemonItemReferenceModel} from "../models/pokemon-item-reference.model";
+import {PokemonItemReferenceModel} from "../../models/pokemon-item-reference.model";
 
 @Injectable({
   providedIn: 'root'
